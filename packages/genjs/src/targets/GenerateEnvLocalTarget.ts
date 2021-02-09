afaeb119476cb97dda22fd95668b265fad4b7783
Olivier Hoareau <oha@greenberets.io>
2021-02-09 13:06:45 +0100

    fix(genjs): fix

--- packages/genjs/src/targets/GenerateEnvLocalTarget.ts
+++ packages/genjs/src/targets/GenerateEnvLocalTarget.ts
@@ -3,12 +3,13 @@ import GenericTarget from './GenericTarget';
 export class GenerateEnvLocalTarget extends GenericTarget {
     buildSteps({prefix, env = '$(env)'}) {
         const binDir = this.buildRelativeToRootPath('node_modules/.bin');
+        const outputsDir = this.buildRelativeToRootPath(`outputs/${env}`);
         if (!prefix) return [
             `${binDir}/generate-vars-from-terraform-outputs ${env} ./terraform-to-vars.json > ./.env.local`,
         ];
         return [
             `${binDir}/env ${prefix}_ > ./.env.local`,
-            `${binDir}/generate-vars-from-terraform-outputs ${env} ./terraform-to-vars.json >> ./.env.local`,
+            `${binDir}/generate-vars-from-terraform-outputs ${outputsDir} ./terraform-to-vars.json >> ./.env.local`,
         ];
     }
 }
