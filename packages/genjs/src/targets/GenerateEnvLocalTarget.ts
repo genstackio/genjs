ab45c4c00bccac650fa7c361d7ab5d5e84de4413
Olivier Hoareau <oha@greenberets.io>
2021-02-11 08:33:53 +0100

    fix(genjs): generate env local target was buggy

--- packages/genjs/src/targets/GenerateEnvLocalTarget.ts
+++ packages/genjs/src/targets/GenerateEnvLocalTarget.ts
@@ -5,7 +5,7 @@ export class GenerateEnvLocalTarget extends GenericTarget {
         const binDir = this.buildRelativeToRootPath('node_modules/.bin');
         const outputsDir = this.buildRelativeToRootPath(`outputs/${env}`);
         if (!prefix) return [
-            `${binDir}/generate-vars-from-terraform-outputs ${env} ./terraform-to-vars.json > ./.env.local`,
+            `${binDir}/generate-vars-from-terraform-outputs ${outputsDir} ./terraform-to-vars.json > ./.env.local`,
         ];
         return [
             `${binDir}/env ${prefix}_ > ./.env.local`,
