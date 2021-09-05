e22915cf666f9ec36618751f5ee3667158e8eb02
Olivier Hoareau <oha@greenberets.io>
2021-09-05 18:20:20 +0200

    fix: bad syntax for foreach in Makefile

--- packages/bundle-aws-lambda/src/helpers/applyDeployMakefileHelper.ts
+++ packages/bundle-aws-lambda/src/helpers/applyDeployMakefileHelper.ts
@@ -21,7 +21,7 @@ export function applyDeployMakefileHelper(t: MakefileTemplate, vars: any, p: IPa
                     'AWS_PROFILE=$(AWS_PROFILE) aws s3 cp $(source_package_file) s3://$(target_s3_bucket)/$(target_s3_key)',
                 ])
                 .addTarget('update-lambda-code', [
-                    '$(foreach f,$(target_lambda_name), AWS_DEFAULT_REGION=$(AWS_DEFAULT_REGION) AWS_PROFILE=$(AWS_PROFILE) aws lambda update-function-code --function-name $(f) --s3-bucket $(target_s3_bucket) --s3-key $(target_s3_key))',
+                    '$(foreach f,$(target_lambda_name), AWS_DEFAULT_REGION=$(AWS_DEFAULT_REGION) AWS_PROFILE=$(AWS_PROFILE) aws lambda update-function-code --function-name $(f) --s3-bucket $(target_s3_bucket) --s3-key $(target_s3_key);)',
                 ])
                 .addTarget('update-lambda-code-of', [
                     'AWS_DEFAULT_REGION=$(AWS_DEFAULT_REGION) AWS_PROFILE=$(AWS_PROFILE) aws lambda update-function-code --function-name $(f) --s3-bucket $(target_s3_bucket) --s3-key $(target_s3_key)',
