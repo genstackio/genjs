014a3e30047ee9ff59dfe6feeef0e0f9e092f1bc
Olivier Hoareau <oha@greenberets.io>
2022-02-23 19:59:14 +0100

    feat(plugin-js-statics): update

--- packages/plugin-js-statics/src/Package.ts
+++ packages/plugin-js-statics/src/Package.ts
@@ -1,123 +1,74 @@
-import {
-    AbstractPackage,
-    GitIgnoreTemplate,
-    LicenseTemplate,
-    MakefileTemplate,
-    ReadmeTemplate,
-    TerraformToVarsTemplate,
-    BuildableBehaviour,
-    DeployableBehaviour,
-    InstallableBehaviour,
-    StartableBehaviour, TestableBehaviour,
-    GenerateEnvLocalableBehaviour,
-} from '@genjs/genjs';
+import {JavascriptPackage} from '@genjs/genjs-bundle-javascript';
 
-export default class Package extends AbstractPackage {
-    protected getBehaviours() {
-        return [
-            new BuildableBehaviour(),
-            new DeployableBehaviour(),
-            new GenerateEnvLocalableBehaviour(),
-            new InstallableBehaviour(),
-            new StartableBehaviour(),
-            new TestableBehaviour(),
-        ]
+export default class Package extends JavascriptPackage {
+    constructor(config: any) {
+        super(config, __dirname);
     }
-    protected getTemplateRoot(): string {
-        return `${__dirname}/../templates`;
-    }
-    // noinspection JSUnusedLocalSymbols,JSUnusedGlobalSymbols
-    protected buildDefaultVars(vars: any): any {
+    protected buildDefaultVars(vars: any) {
         return {
+            ...super.buildDefaultVars(vars),
             project_prefix: 'mycompany',
             project_name: 'myproject',
             target_dir: 'build',
             ignore_target_dir: true,
         };
     }
-    protected async buildDynamicFiles(vars: any, cfg: any): Promise<any> {
+    // noinspection JSUnusedLocalSymbols,JSUnusedGlobalSymbols
+    protected async buildDynamicFiles(vars: any, cfg: any) {
         return {
-            ['LICENSE.md']: this.buildLicense(vars),
-            ['README.md']: this.buildReadme(vars),
-            ['.gitignore']: this.buildGitIgnore(vars),
-            ['Makefile']: this.buildMakefile(vars),
-            ['terraform-to-vars.json']: this.buildTerraformToVars(vars),
+            ...(await super.buildDynamicFiles({licenseFile: 'LICENSE.md', ...vars}, cfg)),
         };
     }
-    protected buildLicense(vars: any): LicenseTemplate {
-        return new LicenseTemplate(vars);
-    }
-    protected buildReadme(vars: any): ReadmeTemplate {
-        return new ReadmeTemplate(vars);
-    }
-    protected buildGitIgnore(vars: any): GitIgnoreTemplate {
-        const t = GitIgnoreTemplate.create(vars)
-            .addComment('See https://help.github.com/articles/ignoring-files/ for more about ignoring files.')
+    protected buildGitIgnore(vars: any) {
+        const t = super.buildGitIgnore(vars)
             .addGroup('dependencies', [
                 '/node_modules', '/.pnp', '.pnp.js',
             ])
             .addGroup('testing', [
                 '/coverage',
-            ]);
+            ])
+            .addGroup('misc', [
+                '.DS_Store',
+                '.env.local', '.env.development.local', '.env.test.local', '.env.production.local',
+                'npm-debug.log*', 'yarn-debug.log*', 'yarn-error.log*'
+            ])
+        ;
         if (vars.ignore_target_dir) {
             t
                 .addGroup('production', [
                     `/${vars.target_dir}`,
                 ])
         }
-        t
-            .addGroup('misc', [
-                '.DS_Store',
-                '.env.local', '.env.development.local', '.env.test.local', '.env.production.local',
-                'npm-debug.log*', 'yarn-debug.log*', 'yarn-error.log*',
-            ])
-            ;
         return t;
     }
-    protected buildMakefile(vars: any): MakefileTemplate {
-        return new MakefileTemplate({options: {npmClient: vars.npm_client}, predefinedTargets: this.predefinedTargets, relativeToRoot: this.relativeToRoot, makefile: false !== vars.makefile, ...(vars.makefile || {})})
+    protected buildMakefile(vars: any) {
+        return super.buildMakefile(vars)
             .addGlobalVar('prefix', vars.project_prefix)
             .addGlobalVar('bucket_prefix', vars.bucket_prefix ? vars.bucket_prefix : `$(prefix)-${vars.project_name}`)
             .addGlobalVar('env', 'dev')
             .addGlobalVar('AWS_PROFILE', `${vars.aws_profile_prefix || '$(prefix)'}-$(env)`)
             .addGlobalVar('bucket', vars.bucket ? vars.bucket : `$(env)-$(bucket_prefix)-${vars.name}`)
-            .addGlobalVar('cloudfront', vars.cloudfront ? vars.cloudfront : `$(AWS_CLOUDFRONT_DISTRIBUTION_ID_${vars.name.toUpperCase()})`)
-            .setDefaultTarget('install')
+            .addGlobalVar('cloudfront', vars.cloudfront ? vars.cloudfront : `$(AWS_CLOUDFRONT_DISTRIBUTION_ID_${vars.name.toUpperCase().replace(/[^A-Z0-9_]+/g, '_')})`)
             .addPredefinedTarget('install', 'js-install')
-            .addPredefinedTarget('build', 'js-build', {sourceLocalEnvLocal: true})
-            .addPredefinedTarget('deploy-code', 'aws-s3-sync', {source: `${vars.target_dir}/`})
+            .addPredefinedTarget('build', 'js-build', {ci: (!!vars.hide_ci) ? 'hidden' : undefined, sourceLocalEnvLocal: vars.sourceLocalEnvLocal})
+            .addPredefinedTarget('deploy-code', 'aws-s3-sync', {source: 'build/'})
             .addPredefinedTarget('invalidate-cache', 'aws-cloudfront-create-invalidation')
             .addMetaTarget('deploy', ['deploy-code', 'invalidate-cache'])
-            .addPredefinedTarget('generate-env-local', 'generate-env-local', {prefix: 'STATICS', mode: vars.env_mode || 'terraform'})
-            .addPredefinedTarget('start', 'js-start', {port: this.getParameter('startPort')})
-            .addPredefinedTarget('test', 'js-test', {ci: true, coverage: true})
+            .addPredefinedTarget('generate-env-local', 'generate-env-local', {prefix: 'STATICS_APP', mode: vars.env_mode || 'terraform'})
+            .addPredefinedTarget('start', 'js-start', {port: this.getParameter('startPort'), sourceLocalEnvLocal: vars.sourceLocalEnvLocal})
+            .addPredefinedTarget('test', 'js-test', {ci: true, coverage: false})
             .addPredefinedTarget('test-dev', 'js-test', {local: true, all: true, coverage: false, color: true})
             .addPredefinedTarget('test-cov', 'js-test', {local: true})
-            .addPredefinedTarget('test-ci', 'js-test', {ci: true})
-            .addExportedVar('CI')
-        ;
+            .addPredefinedTarget('test-ci', 'js-test', {ci: true, coverage: false})
+            ;
     }
-    protected buildTerraformToVars(vars: any): TerraformToVarsTemplate {
-        return new TerraformToVarsTemplate(vars);
-    }
-    protected getTechnologies(): any {
+    protected getTechnologies() {
         return [
-            'make',
+            ...super.getTechnologies(),
             'aws_cli',
             'aws_cloudfront',
             'aws_s3',
             'aws_route53',
-            'node',
-            'es6',
-            'yarn',
-            'nvm',
-            'npm',
-            'markdown',
-            'git',
-            'jest',
-            'prettier',
-            'json',
-            this.vars.publish_image && 'docker',
         ];
     }
 }
