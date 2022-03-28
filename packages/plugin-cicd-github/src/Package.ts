2fc2fa599c50273a7dae9a1e4646ab396cdf778e
Olivier Hoareau <oha@greenberets.io>
2022-03-28 20:02:03 +0200

    feat(plugin-cicd-github): on steroid multi-parallelism

--- packages/plugin-cicd-github/src/Package.ts
+++ packages/plugin-cicd-github/src/Package.ts
@@ -6,12 +6,43 @@ export default class Package extends CicdPackage {
     }
     // noinspection JSUnusedLocalSymbols,JSUnusedGlobalSymbols
     protected buildFilesFromTemplates(vars: any, cfg: any) {
+        return {
+            ...super.buildFilesFromTemplates(vars, cfg),
+            ...this.buildWorkflowsFilesFromTemplates(vars, cfg),
+            ...this.buildProjectsWorkflowsFilesFromTemplates(vars, cfg),
+            ...this.buildParallelWorkflowsFilesFromTemplates(vars, cfg),
+        }
+    }
+    protected buildWorkflowsFilesFromTemplates(vars: any, cfg: any) {
         const c = {workflows: {}, ...(vars.github || {})};
         return Object.entries(c.workflows).reduce((acc, [k, v]) => {
             const fileName = `workflows/${k}.yml`;
             this.hasTemplate(fileName) && (acc[fileName] = true);
             return acc;
-        }, super.buildFilesFromTemplates(vars, cfg));
+        }, {} as any);
+    }
+    protected buildParallelWorkflowsFilesFromTemplates(vars: any, cfg: any) {
+        const c = {parallel_workflows: {}, ...(vars.github || {})};
+        return Object.entries(c.parallel_workflows).reduce((acc, [k, v]) => {
+            const fileName = `workflows/parallel-${k}.yml`;
+            this.hasTemplate(fileName) && (acc[fileName] = [`${fileName}.ejs`, {projects: vars.projectsWorkflows}]);
+            return acc;
+        }, {} as any);
+    }
+    protected buildProjectsWorkflowsFilesFromTemplates(vars: any, cfg: any) {
+        const projectsForProjectsWorkflows = vars.projectsWorkflows || {};
+        const {projects_workflows: projectsWorkflows = {}} = {projects_workflows: undefined, ...(vars.github || {})};
+        if (!projectsWorkflows || !Object.keys(projectsWorkflows).length) return {};
+
+        return Object.entries(projectsWorkflows).reduce((acc, [k, v]) => {
+            const fileName = `workflows/project-${k}.yml`;
+            if (!this.hasTemplate(fileName)) return acc;
+            acc = Object.entries(projectsForProjectsWorkflows).reduce((acc2, [kk, vv]) => {
+                acc2[`workflows/${kk}--${k}.yml`] = [`${fileName}.ejs`, vv];
+                return acc2;
+            }, acc);
+            return acc;
+        }, {} as any);
     }
     protected getTechnologies() {
         return [
--- packages/plugin-cicd-github/src/Plugin.ts
+++ packages/plugin-cicd-github/src/Plugin.ts
@@ -1,10 +1,32 @@
 import Package from './Package';
-import {IGenerator, IPlugin} from '@genjs/genjs';
+import {IGenerator, IPackage, IPlugin, PackageGroup} from '@genjs/genjs';
 import registerCicdBundle from '@genjs/genjs-bundle-cicd';
 
 export default class Plugin implements IPlugin {
+    onPackageCreated(p: IPackage, eventType: string, ctx: {data: any, globalContext: any, group: PackageGroup}): void {
+        if ('.' === p.getName()) return;
+        if ('.github' === p.getName()) return;
+        ctx.globalContext.projectsWorkflows = ctx.globalContext.projectsWorkflows || {};
+        ctx.globalContext.projectsWorkflows[p.getName()] = {
+            project: p.getName(),
+            projectPath: (ctx.group.getDir() === '.') ? p.getName() : `${ctx.group.getDir()}/${p.getName()}`,
+        };
+    }
+    onCicdGithubHydrate(p: IPackage, eventType: string, ctx: any): void {
+        ctx.data.projectsWorkflows = {...(ctx.globalContext.projectsWorkflows || {}), ...(ctx.data.projectsWorkflows || {})};
+    }
+    onProjectHydrate(p: IPackage, eventType: string, ctx: any): void {
+        if ('cicd-github' === p.getPackageType()) return;
+        if ('.' === p.getName()) return;
+        if ('.github' === p.getName()) return;
+        if (!ctx.globalContext.projectsWorkflows[p.getName()]) return;
+        //console.log(ctx.globalContext.projectsWorkflows[p.getName()]);
+    }
     register(generator: IGenerator): void {
         registerCicdBundle(generator);
+        generator.registerPackageEventHook('*', 'created', this.onPackageCreated);
+        generator.registerPackageEventHook('*', 'before_hydrate', this.onProjectHydrate);
+        generator.registerPackageEventHook('cicd-github', 'before_hydrate', this.onCicdGithubHydrate);
         generator.registerPackager('cicd-github', cfg => new Package(cfg));
     }
 }
