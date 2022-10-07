a93c6df6198df8c2747c6bf83eb01d41cef0e88d
Olivier Hoareau <oha@greenberets.io>
2022-10-07 09:48:25 +0200

    fix(bundle-python): fix

--- packages/bundle-python/src/targets/PipInstallTarget.ts
+++ packages/bundle-python/src/targets/PipInstallTarget.ts
@@ -3,7 +3,7 @@ import {GenericTarget} from '@genjs/genjs';
 export class PipInstallTarget extends GenericTarget {
     buildSteps(options: any) {
         return [
-            `python3 -m pip install --disable-pip-version-check --root-user-action=ignore -r requirements.txt`,
+            `python3 -m pip --disable-pip-version-check --root-user-action=ignore install -r requirements.txt`,
         ];
     }
     buildDescription() {
