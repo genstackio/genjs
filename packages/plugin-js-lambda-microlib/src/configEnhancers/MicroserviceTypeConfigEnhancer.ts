94aab64bf6bcc1b4c23f35b0a211d2f6b0e057e9
Olivier Hoareau <oha@greenberets.io>
2023-12-04 09:52:53 +0100

    feat(plugin-registry-base): billable workflows

--- packages/plugin-js-lambda-microlib/src/configEnhancers/MicroserviceTypeConfigEnhancer.ts
+++ packages/plugin-js-lambda-microlib/src/configEnhancers/MicroserviceTypeConfigEnhancer.ts
@@ -43,6 +43,56 @@ export class MicroserviceTypeConfigEnhancer extends AbstractConfigEnhancer {
     mergeIndexes(a: any = {}, b: any = {}) {
         return this.mergeMapOfLists(a, b);
     }
+    mergeObjectsWithAppend(a: any = {}, b: any = {}) {
+        a = Object.entries(a).reduce((acc, [k, v]) => {
+            if ('+' === k.slice(0, 1)) {
+                k = k.slice(1);
+                if (!acc[k]) acc[k] = {};
+                acc[k] = this.mergeObjectsN1(acc[k], v);
+                return acc;
+            }
+            acc[k] = v;
+            return acc;
+        }, {});
+        return Object.entries(b).reduce((acc, [k, v]) => {
+            if ('+' === k.slice(0, 1)) {
+                k = k.slice(1);
+                if (!acc[k]) acc[k] = {};
+                acc[k] = this.mergeObjectsN1(acc[k], v);
+                return acc;
+            }
+            acc[k] = v;
+            return acc;
+        }, a);
+    }
+    mergeObjectsN1(a: any = {}, b: any = {}) {
+        if (!a || 'object' !== typeof a) a = {};
+        if (!b || 'object' !== typeof b) b = {};
+        return Object.entries(b).reduce((acc, [k, v]) => {
+            if ((undefined === acc[k]) || (null === acc[k])) {
+                acc[k] = v;
+                return acc;
+            }
+            if (Array.isArray(v)) {
+                if (Array.isArray(acc[k])) {
+                    acc[k] = [...acc[k], ...v];
+                    return acc;
+                }
+                acc[k] = v;
+                return;
+            }
+            if ('object' === typeof v) {
+                if ('object' === typeof acc[k]) {
+                    acc[k] = {...acc[k], ...v};
+                    return acc;
+                }
+                acc[k] = v;
+                return acc;
+            }
+            acc[k] = v;
+            return acc;
+        }, a);
+    }
 }
 
 export default MicroserviceTypeConfigEnhancer
